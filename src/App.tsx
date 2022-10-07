67fac78311822574d86a739f992f875d8322dde9
sanga.park <sagna.park@epikar.com>
2022-10-07 14:48:23 +0900

    App 함수형 -> React.FC type  형(장단점 있음)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App';
 
-function App() {
+// function App() {
+const App: React.FC = () => {
+  // React.FC 타입으로 지졍 -> 장단점이 있음
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Editttttsssssttt <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,6 +23,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
